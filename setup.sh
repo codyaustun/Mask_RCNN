4a26df62f31636c57ba300b0e24082a74cd0c6ff
MehdiShz <mehdisharif@mehdi-sandbox-0.c.tensorflow-onboarding.internal>
2018-07-11 03:13:10 +0000

    bash script added to install requirments and run the Mask R-CNN training

---
+++ setup.sh
@@ -0,0 +1,36 @@
+set -e
+
+ROOT="$HOME/Mask_RCNN"
+
+if [ ! -d $ROOT ]; then
+  git clone https://github.com/robieta/Mask_RCNN.git
+fi
+cd $ROOT
+
+pip install -r requirements.txt
+python3 setup.py install
+
+if [ ! -d cocoapi ]; then
+  git clone https://github.com/cocodataset/cocoapi.git
+fi
+
+cd cocoapi/PythonAPI
+make
+cd ../..
+
+if [ ! -d pycocotools ]; then
+  ln -s cocoapi/PythonAPI/pycocotools/ .
+fi
+
+
+if [ ! -d $HOME/coco_dataset ]; then
+  gsutil -m cp -r gs://garden-team-scripts/coco_dataset $HOME/
+fi
+
+sudo apt-get install python3-tk
+
+
+if ! (echo $PYTHONPATH | grep "Mask_RCNN"); then export PYTHONPATH="$PYTHONPATH:$HOME/Mask_RCNN"; fi;
+cd ~/Mask_RCNN
+python3 samples/coco/coco.py train --dataset=$HOME/coco_dataset --model=imagenet --logs=$HOME/rcnn_logs \
+		--year=2014 --download=true
