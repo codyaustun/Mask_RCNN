d291a2c40585648104ace094bae4e4dc5e97cf3d
MehdiShz <mehdisharif@mehdi-sandbox-0.c.tensorflow-onboarding.internal>
2018-07-11 22:07:07 +0000

    de-linted

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@ set -e
 
 ROOT="$HOME/Mask_RCNN"
 
-if [ ! -d $ROOT ]; then
+if [[ ! -d $ROOT ]]; then
   git clone https://github.com/robieta/Mask_RCNN.git
 fi
-cd $ROOT
+cd "$ROOT"
 
 pip install -r requirements.txt
 python3 setup.py install
 
-if [ ! -d cocoapi ]; then
+if [[ ! -d cocoapi ]]; then
   git clone https://github.com/cocodataset/cocoapi.git
 fi
 
@@ -18,19 +18,19 @@ cd cocoapi/PythonAPI
 make
 cd ../..
 
-if [ ! -d pycocotools ]; then
+if [[ ! -d pycocotools ]]; then
   ln -s cocoapi/PythonAPI/pycocotools/ .
 fi
 
 
-if [ ! -d $HOME/coco_dataset ]; then
-  gsutil -m cp -r gs://garden-team-scripts/coco_dataset $HOME/
+if [[ ! -d $HOME/coco_dataset ]]; then
+  gsutil -m cp -r gs://garden-team-scripts/coco_dataset "$HOME/"
 fi
 
 sudo apt-get install python3-tk
 
 
-if ! (echo $PYTHONPATH | grep "Mask_RCNN"); then export PYTHONPATH="$PYTHONPATH:$HOME/Mask_RCNN"; fi;
+if ! (echo "$PYTHONPATH" | grep "Mask_RCNN"); then export PYTHONPATH="$PYTHONPATH:$HOME/Mask_RCNN"; fi;
 cd ~/Mask_RCNN
-python3 samples/coco/coco.py train --dataset=$HOME/coco_dataset --model=imagenet --logs=$HOME/rcnn_logs \
+python3 samples/coco/coco.py train --dataset="$HOME/coco_dataset" --model=imagenet --logs="$HOME/rcnn_logs" \
 		--year=2014 --download=true
